0767f03fb287bc5d319b5cb2437278e96e28969c
Adam Vesecký <vesecky.adam@gmail.com>
2018-10-03 17:47:22 +0200

    Remove MI-APH v1

--- src/Components/CompValues.cpp
+++
@@ -1,5 +0,0 @@
-#include "CompValues.h"
-
-// engine specific events
-StrId OBJECT_ADDED("OBJECT_ADDED");
-StrId OBJECT_REMOVED("OBJECT_REMOVED");
--- src/Components/CompValues.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "StrId.h"
-
-// engine specific events
-extern StrId OBJECT_ADDED;
-extern StrId OBJECT_REMOVED;
